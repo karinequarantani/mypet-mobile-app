5a7fa36f24751b585bc7dd3bcb858bc47ac7effb
karine.quarantani <karine.quarantani@petlove.com.br>
2023-04-01 16:36:41 -0300

    fix backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  BASE_URL: 'https://mypet-web.onrender.com',
+  BASE_URL: 'https://mypet-web.onrender.com/',
   PROJECT_TOKEN: 'ccad73b8-b1aa-4bcd-a271-7c09304860d8',
   TUTOR_ID: '0e88972f-77f7-495b-9c02-18a933a689cc'
   //BASE_URL: 'http://localhost:8080/',
