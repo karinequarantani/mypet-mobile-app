243eef5982f9cc872c0539bfebf11d274fb06994
karine.quarantani <karine.quarantani@petlove.com.br>
2023-04-01 16:31:03 -0300

    production deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,12 @@
 
 export const environment = {
   production: false,
-  // BASE_URL: 'https://fake-end.herokuapp.com/fake-api/pets/',
-  BASE_URL: 'http://localhost:8080/',
+  BASE_URL: 'https://mypet-web.onrender.com',
   PROJECT_TOKEN: 'ccad73b8-b1aa-4bcd-a271-7c09304860d8',
-  TUTOR_ID: '98e5b4d8-e9b9-4fa3-9c6e-e4699ed85046'
+  TUTOR_ID: '0e88972f-77f7-495b-9c02-18a933a689cc'
+  //BASE_URL: 'http://localhost:8080/',
+  //PROJECT_TOKEN: 'ccad73b8-b1aa-4bcd-a271-7c09304860d8',
+  //TUTOR_ID: '98e5b4d8-e9b9-4fa3-9c6e-e4699ed85046'
 };
 
 /*
