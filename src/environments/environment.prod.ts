8e03a8224af4e6057a78008b65916f59969c5295
karine.quarantani <karine.quarantani@petlove.com.br>
2023-04-01 16:12:25 -0300

    addin project token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   BASE_URL: 'https://mypet-web.onrender.com',
+  PROJECT_TOKEN: 'ccad73b8-b1aa-4bcd-a271-7c09304860d8',
   TUTOR_ID: '0e88972f-77f7-495b-9c02-18a933a689cc'
 };
